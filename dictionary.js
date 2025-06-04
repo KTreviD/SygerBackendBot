20136c4cd9f1d8eac02d1d987f2291cd38ca9c5e
Carlos <carloscharlie4td@hotmail.com>
2025-06-04 06:49:30 -0600

    Initial commit

---
+++ dictionary.js
@@ -0,0 +1,47 @@
+export const dictionary = {
+    accion: 'En la pantalla principal selecciona la opción ',
+    Noticias: {
+        accion: 'Selecciona la opción',
+        Avisos: {
+            'Ver aviso': 'Selecciona el aviso que te interese ver',
+        },
+        Mensajes: {
+            'Ver mensaje': 'Selecciona el mensaje que quieras ver mas información',
+            'Nuevo mensaje': `Selecciona si al administrador o al guardia, llena la informacion que te pide y da en 'Enviar Mensaje'`
+        },
+        Notificaciones: {
+
+        },
+        Recomendados: {
+
+        },
+    },
+    Visitas: {
+        frecuentes: 'En la pantalla principal, selecciona la opción visitas, seleccionamos la opción frecuentes, ahi se vera la lista de los que temenos, para agregar una Nueva seleccionamos Nueva vista, llenamos los datos que se nos pide de la persona y damos click en aceptar, puedes agregar foto de la galleria, Nombre, placas, proveedor, color de auto, modelo, una vez llenados los datos da click en aceptar, se te mostrara un mensaje de exito de visita creada y creara un codigo qr con los datos de ese visitante, el cual puedes compartir dando click al botón de compartir',
+        Bitacoras_de_acceso:'En la pantalla principal, selecciona el menu visiras, se selecciona la bitacora de acceso, se pueden ver todas las personas registradas como visitas y previsualizaciones de sus accesos, selecciona la visita que deseas, ahi se visualizara el historial de la visita de entradas y salidas con fecha y hora de esa persona.',
+        regulares: 'En la pantalla principal, selecciona la opción regulares para dar de alta visitas que recibiremos durante un periodo de tiempo, para dar de alta una Nueva visita damos click al botón de Nueva visita, y se muestra como sugerencia para capturer todos losd atos, ya que esto agiliza el acceso de nuetra visita, da click en aceptar, se muestra pantalla para capturer los datos y lo llenas con nombre, eliges la fecha apartir que quieres otorgar el acceso, seleccionas la cantidad de dias que quieres que sea el acceso, llenas la info como placas, proveedor, color de auto, modelo, da click en aceptar, verifica los datos, te saldra un mensaje de exito y un qr el cual puedes compartir, , y ahi puedes ver la visita que creaste en tu menu sin salirte de visitor, si quieres ver los datos y bitacora le puedes dar en bitacora y ahi visualizaras los datos de entrada y salida.',
+        empleado_domestico: 'En la pantalla principal, selecciona visitas, selecciona la opción de empleado domestico, se muestra la lista de empleados previamente registrados, para dar de alta nuevo empleado selecciona la opción Nueva vista, aparece, registra la informacion que se solicita del empleado. verifica los datos y da click en generar vista, te aparecera mensaje de exito y un qr para poder compartir con los datos de la visita, si seleccionas uno puedes ver sus datos, y modificarlos, por ejemplo seleccionar que viene otro dia que no tenia seleeccionado y te saldra un mensaje de que a sido actualizado y Podemos comprtir su codigo qr con la informacion actualizada, si le damos en bitacora de acceso Podemos ver todo su historial de entradas y salidas con fecha y hora, Tambien puedes borrarlo dandole al botón de borrar empleado.',
+        servicio_a_domicilio_acceso_unico: 'En la pantalla principal, selecciona la opción de visitas, muestra el menu de visitas, seleccionas la opción de servicio a domicilio acceso unico, entras a la pantalla y le das click a generar Nueva visita, aparece sugerencia de que al capturer toda la informacion se agiliza la entrada de las visitas, capturas los datos para el registro, como: Nombre, seleccionas la fecha, das click en aceptar, y continuas llenando la informacion como placas, proveedor, color de auto y modelo, das click en aceptar, te aparecera un mensaje de exito para el buen registrado, se muestra el codigo qr con los siguientes datos: visita,te, fecha de visita, vigencia, domicilio, leyenda de pase de un solo acceso y su codigo qr que puedes compartir, veras el registro de la visita creada, si le das click podras ver todos sus datos y si le das click a borrar podras borrar esa visita',
+    },
+    Encuestas: {
+        ver_encuestas: `En la pantalla principal, selecciona la opción 'Encuestas', selecciona la encuesta que quieras ver mas detalles.`,
+        contestar_encuestas: `En la pantalla principal, selecciona la opción 'Encuestas', veras la lista de encuestas, selecciona la encuesta que quieres responder, llena sus datos y selecciona el boton de 'Finalizada', saldra un aviso que te advierte que una vez respondida la encuesta no podras cambiar la respuesta, selecciona 'Cancelar', para no guardar, o 'Aceptar', para guardar las respuestas en la encuesta.`,
+    }, 
+    Reservaciones: {
+        agregar_reservacion: `En la pantalla principal, selecciona la opción 'Reservaciones', se mostrara la lista de espacios disponibles a reservar, selecciona el que desees reservas, selecciona la fecha y la hora, despues da click en el boton de reservar, se abrira la pantalla para que puedas pagar para reservar el espacio, llena el campo folio con el numero de transaccion del pago y da click en el boton de reservar, para despues agregar una imagen del comprobante de pago y dar click en agregar pago.`,
+        ver_reservacion: `En la pantalla principal, selecciona la opción 'Reservaciones', despues da click al boton 'Mis reservaciones', ahi podras ver tus reservaciones registradas, las verdes es que ya las valido la administracion, las rojas es que siguen pendientes.`,
+        ver_reglamento: `En la pantalla principal, selecciona la opción 'Reservaciones', despues selecciona el espacio en el que te interese ver su reglamento, y al entrar dale click al boton 'Reglamento'.`,
+    },
+    'Botón panico': {
+        activar_botón_panico:'En la pantalla principal, selecciona botón panico, para activar presiona botón rojo 3 dos',
+        donde_ver_mis_contactos: `En la pantalla principal, selecciona botón panico, selecciona opción 'Mis contactos' para seleccionar los contactos que recibirán la alerta de emergencia, contactos aceptados son los que aceptaron recibir la alerta, contactos pendientes son los que se les envio la envitacion pero aun no la han aceptado.`,
+        agregar_nuevo_contacto_panico: `En la pantalla principal, selecciona botón panico, selecciona opción 'Mis contactos', selecciona la opción 'Agregar contacto', para añadir uno nuevo, la app acepta máximo 5 contactos, visualizaras la lista de contactos a quienes puedes enviar la invitación, seleccionas uno y se agregara a contactos pendientes.`,
+        soy_contacto_de: `En la pantalla principal, selecciona botón panico, selecciona opción 'Soy contacto de', ahi veras la lista de quien te tiene en su lista de contactos en situacion de panico`,
+        aceptar_ser_contacto: `En la pantalla principal, selecciona botón panico, selecciona opción 'Soy contacto de', ahi veras la lista de que invitaciones para ser contacto en situacion de panico tienes, solo selecciona la opción de aceptar o de rechazar, y acepta la notificacion que te aparecera de tu telefono para que se agregue exitosamente`,
+    },
+    'Mi cuenta': {
+        agregar_pago: `En la pantalla principal, selecciona la opcion 'Agregar Pago', llena los datos que se piden y da click en el boton de siguiente.`,
+        ver_pagos: `En la pantalla principal, selecciona la opcion 'Mis pagos', podras ver la lista de historial de pagos realizados, si quieres ver mas detalles selecciona el comprobante de pago que te interese.`,
+        ver_estado_de_cuenta: `En la pantalla principal, selecciona la opcion 'Mi estado de cuenta', podras seleccionar el periodo del cual te interese ver el estado de cuenta.`
+    }
+}
